ec72fd9fda251695f4183b9116d715422c5c939f
LAPTOP-RC7BFFPH\HP <zelaf44923@gmail.com>
2023-11-02 00:51:00 +0300

    feat: add class Cinema

---
+++ lab3/src/Cinema.java
@@ -0,0 +1,34 @@
+public class Cinema {
+    public Cinema(String nameOfCinema, String address, int numberOfHalls){
+        this.NameOfCinema = nameOfCinema;
+        this.Address = address;
+        this.NumberOfHalls = numberOfHalls;
+    }
+    public String getNameOfCinema(){
+        return NameOfCinema;
+    }
+
+    public void setNameOfCinema(String nameOfCinema){
+        this.NameOfCinema = nameOfCinema;
+    }
+
+    public String getAddress(){
+        return Address;
+    }
+
+    public void setAddress(String address){
+        this.Address = address;
+    }
+
+    public int getNumberOfHalls(){
+        return NumberOfHalls;
+    }
+
+    public void setNumberOfHalls(int numberOfHalls){
+        this.NumberOfHalls = numberOfHalls;
+    }
+
+    private String NameOfCinema;
+    private String Address;
+    private int NumberOfHalls;
+}
